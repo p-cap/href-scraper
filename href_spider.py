9888919d2a5b1e04b0dd7fda3f1e78a9bc8bdd97
Paul Capili <64616459+p-cap@users.noreply.github.com>
2021-05-26 14:35:14 -1000

    Update href_spider.py

--- href_spider.py
+++ href_spider.py
@@ -1,22 +1,19 @@
-import sys
 import scrapy
-
 
 class HrefSpider(scrapy.Spider):
     name = 'href_links'
     
     # makes requests to the urls defined inside the given variable
     start_urls = [ 
-       #Enter URL to scrape here
+       #>>> Insert URL here <<<#
     ]   
-   
+
     # called the default callback
     def parse(self, response):
-        # loop through the quote elements using a CSS Selector
+        
+        # loop through the a elements 
         for api in response.css('a'):
             # yield a Python dict
             yield {
                 'APIs': api.attrib['href']
             }   
-
-
