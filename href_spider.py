ba3d3e796656eb2068bfc9d7db76c8d144fcc6ae
Paul Capili <64616459+p-cap@users.noreply.github.com>
2021-05-26 14:26:25 -1000

    Update href_spider.py

--- href_spider.py
+++ href_spider.py
@@ -7,7 +7,7 @@ class HrefSpider(scrapy.Spider):
     
     # makes requests to the urls defined inside the given variable
     start_urls = [ 
-       'https://www.twilio.com/login?g=%2Fconsole%3F&t=2b1c98334b25c1a785ef15b6556396290e3c704a9b57fc40687cbccd79c46a8c',
+       #Enter URL to scrape here
     ]   
    
     # called the default callback
